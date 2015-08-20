c470b5ab95b045d5eb0c9baa656edc8a1bccb495
Erivando Sena <erivandosena@gmail.com>
2015-08-20 16:50:14 -0300

    Desenvolvendo api webservice RESTful da catraca.

--- catraca/restful/servidor.py
+++ catraca/restful/servidor.py
@@ -5,17 +5,20 @@
 
 
 from flask import Flask, jsonify, abort, make_response
-from flask.ext.restful import Api, Resource, reqparse, fields, marshal, marshal_with_field
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from flask.ext.httpauth import HTTPBasicAuth
+from catraca.dao.tipodao import TipoDAO
+from catraca.dao.tipo import Tipo
 import json
 import decimal, simplejson
-
-from catraca.dao.tipodao import TipoDAO
-
+#from catraca.dao.mensagemdao import MensagemDAO
 
 app = Flask(__name__, static_url_path="")
 api = Api(app)
 auth = HTTPBasicAuth()
+
+tipo = Tipo()
+tipo_dao = TipoDAO()
 
 @auth.get_password
 def get_password(username):
@@ -30,63 +33,44 @@ def unauthorized():
     # auth dialog
     return make_response(jsonify({'message': 'Acesso negado.'}), 403)
 
-tipo = TipoDAO().busca(1)
-
-#print json.dumps(tipo.__dict__ ,default=decimal_default,sort_keys=True,indent=4,separators=(',', ': '))
-
 class DecimalJSONEncoder(simplejson.JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
             return str(o)
         return super(DecimalJSONEncoder, self).default(o)
+#     
+# #tipo = MensagemDAO().busca()
+# 
+# #print json.dumps(tipo.__dict__ ,default=decimal_default,sort_keys=True,indent=4,separators=(',', ': '))
+# #tasks = json.dumps(tipo.__dict__, sort_keys=True, indent=4, cls=DecimalJSONEncoder)
+# #tasks = simplejson.dumps(tipo.__dict__, sort_keys=True, indent=4)
 
-#tasks = json.dumps(tipo.__dict__, sort_keys=True, indent=4, cls=DecimalJSONEncoder)
-tasks = simplejson.dumps(tipo.__dict__, sort_keys=True, indent=4)
-
-
-print tasks
-
-# tasks_teste = [
-#  
-#     {
-#         '_Tipo__tipo_id': 1,
-#         '_Tipo__tipo_nome': "TESTE",
-#         '_Tipo__tipo_vlr_credito': 1.10
-#     }
-# ]
-#  
-# print tasks_teste
- 
-#tasks = tasks_teste
-
-#tasks = json.dumps(tipo.__dict__,use_decimal=True)
-
-# tasks = json.dumps(tipo, skipkeys=False, ensure_ascii=True, 
-#            check_circular=True, allow_nan=True, cls=None, 
-#            indent=None, separators=None, encoding='utf-8', 
-#            default=None, **kw)
+def obtem_lista(lista):
+    ilista=[]
+    for item in lista:
+        i = {
+            'id':item[0],
+            'title':item[1],
+            'description':item[2]
+        }
+        ilista.append(i)
+    return ilista
 
 # tasks = [
 #     {
 #         'id': 1,
-#         'title': u'teste 1',
-#         'description': u'UM, DOIS, TR�S, QUATRO, CINCO.',
+#         'title': u'Buy groceries',
+#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
 #         'done': False
-#     },
+#      },
 #     {
 #         'id': 2,
-#         'title': u'Teste 02',
-#         'description': u'1� teste com WebService RESTful.',
-#         'done': False
-#     },
-#     {
-#         'id': 3,
-#         'title': str(tipo.nome),
-#         'description': str(tipo.valor),
+#         'title': u'Learn Python',
+#         'description': u'Need to find a good Python tutorial on the web',
 #         'done': False
 #     }
 # ]
-
+# 
 # task_fields = {
 #     'title': fields.String,
 #     'description': fields.String,
@@ -94,20 +78,17 @@ print tasks
 #     'uri': fields.Url('task')
 # }
 
+tasks = obtem_lista(tipo_dao.busca())
+
+print tasks
+
 task_fields = {
-    '_Tipo__tipo_id': fields.Integer,
-    '_Tipo__tipo_nome': fields.String,
-    '_Tipo__tipo_vlr_credito': fields.Float,
+    #'id': fields.Integer,
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
     'uri': fields.Url('task')
-
 }
-
-# task_fields = {
-#     '_Tipo__tipo_id': fields.Integer,
-#     '_Tipo__tipo_nome': fields.String,
-#     '_Tipo__tipo_vlr_credito': fields.Float,
-#     'uri': fields.Url('task')
-# }
 
 
 class TaskListAPI(Resource):
@@ -115,26 +96,32 @@ class TaskListAPI(Resource):
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        #self.reqparse.add_argument('_Tipo__tipo_id', type=str, default="", location='json')
-        #self.reqparse.add_argument('_Tipo__tipo_nome', type=str, required=True, help='Sem nome fornecido.', location='json')
-        #self.reqparse.add_argument('_Tipo__tipo_vlr_credito', type=str, default="", location='json')
+        self.reqparse.add_argument('title', type=str, required=True, help='No task title provided', location='json')
+        self.reqparse.add_argument('description', type=str, default="", location='json')
         super(TaskListAPI, self).__init__()
 
-    @marshal_with_field(fields.List(fields.Integer))
+
     def get(self):
-        print {'tasks': [marshal(task, task_fields) for task in tasks]}
-        return {'tasks': [marshal(task, task_fields) for task in tasks]}
- 
+        print "SELECIONOU"
+        #return {'tasks': [marshal(task, task_fields) for task in tasks]}
+        return {'tasks': map(lambda t: marshal(t, task_fields), tasks)}
+
     def post(self):
         args = self.reqparse.parse_args()
         task = {
-            #'id': tasks[-1]['id'] + 1,
-            'id': tasks['id'],
-            'nome': args['nome'],
-            'valor': args['valor']#,
-            #'done': False
+            'id': tasks[-1]['id'] + 1,
+            'title': args['title'],
+            'description': args['description'],
+            'done': False
         }
+        #tasks.append(task)
+        tipo = Tipo()
+        tipo.nome = args['title']
+        tipo.valor = args['description']
+        tipo_dao.mantem(tipo,False)
+        print tipo_dao.aviso
         tasks.append(task)
+        print "INSERIU"
         return {'task': marshal(task, task_fields)}, 201
 
 
@@ -143,18 +130,17 @@ class TaskAPI(Resource):
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        #self.reqparse.add_argument('_Tipo__tipo_nome', type=str, location='json')
-        #self.reqparse.add_argument('_Tipo__tipo_vlr_credito', type=str, location='json')
-        #self.reqparse.add_argument('done', type=bool, location='json')
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('done', type=bool, location='json')
         super(TaskAPI, self).__init__()
 
     def get(self, id):
         task = [task for task in tasks if task['id'] == id]
         if len(task) == 0:
             abort(404)
-        print {'task': marshal(task[0], task_fields)}
+        print "SELECT"
         return {'task': marshal(task[0], task_fields)}
- 
 
     def put(self, id):
         task = [task for task in tasks if task['id'] == id]
@@ -165,6 +151,14 @@ class TaskAPI(Resource):
         for k, v in args.items():
             if v is not None:
                 task[k] = v
+                
+#         tipo = Tipo()
+#         tipo.nome = args['title']
+#         tipo.valor = args['description']
+#         tipo_dao.mantem(tipo,False)
+#         print tipo_dao.aviso
+                
+        print "EDITOU"
         return {'task': marshal(task, task_fields)}
 
     def delete(self, id):
@@ -172,12 +166,12 @@ class TaskAPI(Resource):
         if len(task) == 0:
             abort(404)
         tasks.remove(task[0])
+        print "EXCLUIU"
         return {'result': True}
 
 
 api.add_resource(TaskListAPI, '/api/catraca', endpoint='tasks')
 api.add_resource(TaskAPI, '/api/catraca/<int:id>', endpoint='task')
 
-
 if __name__ == '__main__':
-    app.run(host='192.168.1.253', port=8089, debug=True)
+    app.run(debug=True)
