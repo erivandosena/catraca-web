5aa5bbbe7be8dd1aba2fd34179d35d9b3e3cc94a
Erivando Sena <erivandosena@gmail.com>
2015-09-26 18:02:59 -0300

    Remocao do commit do Author: Jefferson Uchoa Ponte com descricao: "Foi" irregular para o historico.

--- catraca/dispositivos/sensoroptico.py
+++ catraca/dispositivos/sensoroptico.py
@@ -2,6 +2,7 @@
 # -*- coding: latin-1 -*-
 
 
+import subprocess
 from time import sleep
 from catraca.logs import Logs
 from catraca.pinos import PinoControle
@@ -40,72 +41,85 @@ class SensorOptico(object):
         elif sensor == 2:
             return self.rpi.estado(self.sensor_2)
     
-    def registra_giro(self, tempo):
+    def registra_giro(self, tempo, catraca):
         codigo_giro = ''
         finaliza_giro = True
         try:
-            # cronômetro regressivo para o giro
-            for segundo in range(tempo, -1, -1):
-                self.tempo_decorrido = (tempo/1000)-(segundo/1000)
-                #print str(self.tempo_decorrido)+" de "+str(tempo/1000)
-                self.log.logger.debug(str(segundo ) + ' seg. restantes para expirar o tempo para giro.')
-                self.log.logger.debug('Catraca em repouso, codigo sensores: '+ str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2)))
-                """
-                # GIRO HORARIO
-                if self.ler_sensor(1) == 1 and self.ler_sensor(2) == 0:
-                    self.log.logger.info('Girou catraca no sentido horario.')
-                    while self.ler_sensor(1) == 1 and self.ler_sensor(2) == 0:
+            # GIRO HORARIO
+            if catraca.operacao == 1:
+                for segundo in range(tempo, -1, -1):
+                    self.tempo_decorrido = (tempo/1000)-(segundo/1000)
+                    #print str(self.tempo_decorrido)+" de "+str(tempo/1000)
+                    self.log.logger.debug(str(segundo ) + ' seg. restantes para expirar o tempo para giro.')
+                    self.log.logger.debug('Catraca em repouso, codigo sensores: '+ str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2)))
+                    if self.ler_sensor(1) == 1 and self.ler_sensor(2) == 0:
                         codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
-                        self.log.logger.debug('Continuou o giro horario, codigo sensores: '+ str(codigo_giro))
-                        codigo_giro = ''
-                    while self.ler_sensor(1) == 1 and self.ler_sensor(2) == 1:
-                        codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
-                        self.log.logger.debug('No meio do giro horario, codigo sensores: '+ str(codigo_giro))
-                    while self.ler_sensor(1) == 0 and self.ler_sensor(2) == 1:
-                        codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
-                        self.log.logger.debug('Finalizando o giro horario, codigo sensores: '+ str(codigo_giro))
-                    if codigo_giro == '01': 
-                        codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
-                        self.log.logger.info('Giro horario finalizado em '+ str(segundo/1000)+' segundo(s).')
-                        self.log.logger.debug('Giro horario completo no codigo: '+ str(codigo_giro))
-                        return True
-                    elif codigo_giro == '00': 
-                        codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
-                        self.log.logger.info('Giro horario finalizado em '+ str(segundo/1000)+' segundo(s).')
-                        self.log.logger.debug('Giro horario completo no codigo: '+ str(codigo_giro))
-                        return True
-                # GIRO ANTIHORARIO
-                """
-                #el
-                if self.ler_sensor(2) == 1 and self.ler_sensor(1) == 0:
-                    self.log.logger.info('Girou a catraca no sentido antihorario.')
-                    while self.ler_sensor(2) == 1 and self.ler_sensor(1) == 0:
+                        self.log.logger.info('Girou catraca no sentido horario, codigo sensores: '+ str(codigo_giro))
+                        while self.ler_sensor(1) == 1 and self.ler_sensor(2) == 0:
+                            codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
+                            self.log.logger.debug('Continuou o giro horario, codigo sensores: '+ str(codigo_giro))
+                            codigo_giro = ''
+                        while self.ler_sensor(1) == 1 and self.ler_sensor(2) == 1:
+                            codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
+                            self.log.logger.debug('No meio do giro horario, codigo sensores: '+ str(codigo_giro))
+                            #self.alerta_sonoro(10,codigo_giro)
+                        while self.ler_sensor(1) == 0 and self.ler_sensor(2) == 1:
+                            codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
+                            self.log.logger.debug('Finalizando o giro horario, codigo sensores: '+ str(codigo_giro))
+                        if codigo_giro == '01': 
+                            codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
+                            self.log.logger.info('Giro horario finalizado em '+ str(self.tempo_decorrido)+' segundo(s).')
+                            self.log.logger.debug('Giro horario completo no codigo: '+ str(codigo_giro))
+                            finaliza_giro = False
+                            #print 'Finalizou no girou 01'
+                            return finaliza_giro
+                        elif codigo_giro == '00': 
+                            codigo_giro = str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2))
+                            self.log.logger.info('Giro horario finalizado em '+ str(self.tempo_decorrido)+' segundo(s).')
+                            self.log.logger.debug('Giro horario completo no codigo: '+ str(codigo_giro))
+                            finaliza_giro = False
+                            #print 'Finalizou no girou 00'
+                            return finaliza_giro
+                return True
+            # GIRO ANTIHORARIO
+            if catraca.operacao == 2:
+                for segundo in range(tempo, -1, -1):
+                    self.tempo_decorrido = (tempo/1000)-(segundo/1000)
+                    #print str(self.tempo_decorrido)+" de "+str(tempo/1000)
+                    self.log.logger.debug(str(segundo ) + ' seg. restantes para expirar o tempo para giro.')
+                    self.log.logger.debug('Catraca em repouso, codigo sensores: '+ str(self.ler_sensor(1)) + '' + str(self.ler_sensor(2)))
+                    if self.ler_sensor(2) == 1 and self.ler_sensor(1) == 0:
                         codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
-                        self.log.logger.debug('Continuou o giro antihorario, codigo sensores: '+ str(codigo_giro))
-                        codigo_giro = ''
-                    while self.ler_sensor(2) == 1 and self.ler_sensor(1) == 1:
-                        codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
-                        self.log.logger.debug('No meio do giro antihorario, codigo sensores: '+ str(codigo_giro))
-                    while self.ler_sensor(2) == 0 and self.ler_sensor(1) == 1:
-                        codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
-                        self.log.logger.debug('Finalizando o giro antihorario, codigo sensores: '+ str(codigo_giro))
-                    if codigo_giro == '01':
-                        codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
-                        self.log.logger.info('Giro antihorario finalizado em '+ str(self.tempo_decorrido)+' segundo(s).')
-                        #self.log.logger.debug('Giro antihorario completo no codigo: '+ str(codigo_giro))
-                        finaliza_giro = False
-                        #print 'Finalizou no girou 01'
-                        return finaliza_giro
-                    elif codigo_giro == '00':
-                        codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
-                        self.log.logger.info('Giro antihorario finalizado em '+ str(self.tempo_decorrido)+' segundo(s).')
-                        #self.log.logger.debug('Giro antihorario completo no codigo: '+ str(codigo_giro))
-                        finaliza_giro = False
-                        #print 'Finalizou no girou 00'
-                        return finaliza_giro
-
-#                 finaliza_giro = False
-            return True
+                        self.log.logger.info('Girou a catraca no sentido antihorario, codigo sensores: '+ str(codigo_giro))
+                        while self.ler_sensor(2) == 1 and self.ler_sensor(1) == 0:
+                            codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
+                            self.log.logger.debug('Continuou o giro antihorario, codigo sensores: '+ str(codigo_giro))
+                            #self.alerta_sonoro(10,codigo_giro)
+                            codigo_giro = ''
+                        while self.ler_sensor(2) == 1 and self.ler_sensor(1) == 1:
+                            codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
+                            #self.alerta_sonoro(10,codigo_giro)
+                            self.log.logger.debug('No meio do giro antihorario, codigo sensores: '+ str(codigo_giro))
+                        while self.ler_sensor(2) == 0 and self.ler_sensor(1) == 1:
+                            codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
+                            self.log.logger.debug('Finalizando o giro antihorario, codigo sensores: '+ str(codigo_giro))
+                            #self.alerta_sonoro(10,codigo_giro)
+                        if codigo_giro == '01':
+                            codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
+                            self.log.logger.info('Giro antihorario finalizado em '+ str(self.tempo_decorrido)+' segundo(s).')
+                            self.log.logger.debug('Giro antihorario completo no codigo: '+ str(codigo_giro))
+                            finaliza_giro = False
+                            #print 'Finalizou no girou 01'
+                            return finaliza_giro
+                        elif codigo_giro == '00':
+                            codigo_giro = str(self.ler_sensor(2)) + '' + str(self.ler_sensor(1))
+                            self.log.logger.info('Giro antihorario finalizado em '+ str(self.tempo_decorrido)+' segundo(s).')
+                            self.log.logger.debug('Giro antihorario completo no codigo: '+ str(codigo_giro))
+                            finaliza_giro = False
+                            #print 'Finalizou no girou 00'
+                            return finaliza_giro
+                return True
+            return False
         except SystemExit, KeyboardInterrupt:
             raise
         except Exception:
@@ -113,3 +127,24 @@ class SensorOptico(object):
         finally:
             pass
         
+    def obtem_codigo_sensores(self,s1,s2):
+        return str(self.ler_sensor(s1)) + '' + str(self.ler_sensor(s2))
+        
+    def beep(sel, mp3):
+        process = subprocess.Popen(['mpg321 -q -g 100 '+mp3, '-R player'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #STDOUT
+        stdout_value = process.communicate()[0]
+        
+    def alerta_sonoro(self, tempo, codigo): 
+        contador = 0
+        while contador < tempo:
+            sleep(1)
+            contador += 1
+            print str(contador)
+        else:
+            while (codigo == '11'):
+                self.beep('audio/beep-04.mp3')
+                print 'alertando...'
+                if codigo == '00':
+                    break
+                sleep(1)
+                
