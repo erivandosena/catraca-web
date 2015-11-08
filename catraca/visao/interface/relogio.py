434cb4db3e4f1778fc60056f4798f0d33b8286c1
Erivando Sena <erivandosena@gmail.com>
2015-11-08 00:01:02 -0300

    Melhorias na classe da thread que processa a atividade dos turnos.

--- catraca/visao/interface/relogio.py
+++ catraca/visao/interface/relogio.py
@@ -29,17 +29,19 @@ class Relogio(threading.Thread):
     catraca_dao = CatracaDAO()
     
     __periodo_ativo = None
-    __hora_sistema = datetime.datetime.strptime('00:00:00','%H:%M:%S').time()
-    __datahora_display = datetime.datetime.now().strftime('%d/%B/%Y\n    %H:%M:%S')
-    hora_atual = datetime.datetime.strptime('00:00:00','%H:%M:%S').time()
-    hora_inicio = datetime.datetime.strptime('00:00:00','%H:%M:%S').time()
-    hora_fim = datetime.datetime.strptime('00:00:00','%H:%M:%S').time()
+    __hora_sistema = None
+    __datahora_display = None
+    hora_atual = None
+    hora_inicio = None
+    hora_fim = None
     turno_ativo = None
+    cronometro = 0
     
     def __init__(self, intervalo=1):
         super(Relogio, self).__init__()
         self.__hora_sistema = self.util.obtem_hora()
-        self.__datahora_display = self.util.obtem_hora()
+        self.__datahora_display = self.util.obtem_datahora_display()
+        self.hora_atual = self.util.obtem_hora()
         threading.Thread.__init__(self)
         self.name = 'Thread Relogio'
         self.intervalo = intervalo
@@ -66,17 +68,25 @@ class Relogio(threading.Thread):
         cliente_restful = ClienteRestful()
         cliente_restful.start()
         while True:
-            #self.aviso.exibir_datahora(self.data_hora_display,0)  
             # atualiza variaveis com hora do sistema
             self.hora = self.util.obtem_hora()
+            self.hora_atual = self.hora
             self.data_hora_display = self.util.obtem_datahora_display()
+            
             # atualiza thread com hora atual
             cliente_restful.hora_real = self.hora
-            #cliente_restful.data_hora_real = self.data_hora_display
+            cliente_restful.data_hora_real = self.data_hora_display
+            # escerve data/hora no display
+            self.aviso.exibir_datahora(self.data_hora_display,0)  
             
+            # verifica a disponibilidade de turnos
             self.periodo = self.obtem_periodo()
-            print self.periodo
             
+            print self.hora
+            print self.turno_ativo
+            
+            print self.periodo
+
             sleep(self.intervalo)
               
     @property
@@ -107,33 +117,31 @@ class Relogio(threading.Thread):
         return self.catraca_dao.busca_por_ip(self.util.obtem_ip())
     
     def obtem_turno(self):
-        self.turno_ativo = self.turno_dao.busca_por_catraca(self.obtem_catraca(), self.hora)
-        print "nova consulta no turno!"
-        if self.turno_ativo:
-            self.hora_inicio = datetime.datetime.strptime(str(self.turno_ativo[1]),'%H:%M:%S').time()
-            self.hora_fim = datetime.datetime.strptime(str(self.turno_ativo[2]),'%H:%M:%S').time()       
-            print self.turno_ativo      
-            return self.turno_ativo
+        p1 = datetime.datetime.strptime('05:59:59','%H:%M:%S').time()
+        p2 = datetime.datetime.strptime('22:29:59','%H:%M:%S').time()
+        if (self.hora_atual > p1) and (self.hora_atual < p2):
+            self.turno_ativo = self.turno_dao.busca_por_catraca(self.obtem_catraca(), self.hora)
+            print "novo select no turno!"
+            if self.turno_ativo:
+                self.hora_inicio = datetime.datetime.strptime(str(self.turno_ativo[1]),'%H:%M:%S').time()
+                self.hora_fim = datetime.datetime.strptime(str(self.turno_ativo[2]),'%H:%M:%S').time()       
+                return self.turno_ativo
+            else:
+                return None
         else:
             self.turno_ativo = None
+            return None
             
+    def obtem_temporizador(self):
+        return False
+    
     def obtem_periodo(self):
-        self.hora_atual = self.hora
-        if (((self.hora_atual >= self.hora_inicio) and (self.hora_atual <= self.hora_fim)) or ((self.hora_atual >= self.hora_inicio) and (self.hora_atual <= self.hora_fim)) or (self.turno_ativo is None)):
-            if self.turno_ativo:
+        self.obtem_turno()
+        if self.turno_ativo:
+            if ((self.hora_atual >= self.hora_inicio) and (self.hora_atual <= self.hora_fim)) or ((self.hora_atual >= self.hora_inicio) and (self.hora_atual <= self.hora_fim)):  
                 return True
             else:
-                self.obtem_turno()
-                return True
+                return False
         else:
-            return False
-#                     if not (((self.hora_atual >= self.hora_inicio) and (self.hora_atual <= self.hora_fim)) or ((self.hora_atual >= self.hora_inicio) and (self.hora_atual <= self.hora_fim))):
-#                         return False
-#                     else:
-#                         return True
-                
-#     def obtem_periodo(self):
-#         if not (((self.hora >= self.hora_inicio) and (self.hora <= self.hora_fim)) or ((self.hora >= self.hora_inicio) and (self.hora <= self.hora_fim))):
-#             return False
-#         else:
-#             return True
+            return False 
+            
