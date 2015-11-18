9bda28ff515c64bc281b44214313c37094bdf28f
Alan Cleber Morais Gomes <kleber.acmg@gmail.com>
2015-11-18 14:11:35 -0300

    Atualização Menu.

--- web/web designer/index.php
+++ web/web designer/index.php
@@ -1,8 +1,5 @@
-
-
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 	<head>
 
 		<meta charset="UTF-8">
@@ -15,13 +12,13 @@
 		<link rel="stylesheet" href="css/estilo.css" type="text/css" media="screen">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>		
-		<script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>
+		<script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>		
 		<script src="js/bootstrap.js"></script>
 
 	</head>
 
 	<body >
-		<div class="pagina">			
+		<div class="pagina fundo-cinza1">
 			<div id="barra-governo">
 			    <div class="resolucao config">
 			       <div class="a-esquerda">
@@ -38,26 +35,17 @@
 			    </div>
 			</div>
 
-			<div class="doze colunas banner gradiente">
-					
-				<div id="acessibilidade" class="">
-				    <div class="resolucao config">
-				        <a href="#conteudo" tabindex="1" accesskey="1">Ir para o conteúdo <b>1</b></a>
-				        <a href="#menu" tabindex="2" accesskey="2">Ir para o menu <b>2</b></a>
-				        <a href="#busca" tabindex="3" accesskey="3">Ir para a busca <b>3</b></a>
-				        <a href="#rodape" tabindex="4" accesskey="4"><span>Ir para o rodapé <b>4</b></a>
-				    </div>
-				</div>
+			<div class="doze colunas banner gradiente">			
 
 				<div id="topo" class="resolucao config">
 					<div class="tres colunas">
-						<a href="http://www.dti.unilab.edu.br"><img class="imagem-responsiva" src="http://dti.unilab.edu.br/wp-content/themes/dti/img/logo_h-site.png" alt=""></a>				
+						<a href="http://www.dti.unilab.edu.br"><img class="imagem-responsiva" src="img/logo_h-site.png" alt=""></a>				
 					</div>
 					<div class="seis colunas centralizado">
-						<h1 class="texto-branco">CATRACA<br><small class="texto-branco">Controle Administrativo de Tráfego Acadêmico Automatizado</small></h1>
+						<h1>CATRACA<br><small class="texto-branco">Controle Administrativo de Tráfego Acadêmico Automatizado</small></h1>
 					</div>
 					<div class="tres colunas alinhado-a-direita">
-						<a href="http://www.unilab.edu.br"><img class="imagem-responsiva centralizada" src="http://200.129.19.10/pub/templates_dti/img/logo-unilab-branco.png" alt=""></a>
+						<a href="http://www.unilab.edu.br"><img class="imagem-responsiva centralizada" src="img/logo-unilab-branco.png" alt=""></a>
 					</div>			
 				</div>
 			</div>
@@ -70,36 +58,39 @@
 					<a href="#"><span>Dados Abertos |</span></a>
 					<a href="#"><span>Área de Imprensa |</span></a>
 				</div>
-			</div>		
-						
-			<div class="doze colunas centralizado">
-				<img class="fundo" src="https://upload.wikimedia.org/wikipedia/commons/0/07/S%C3%ADmbolo_da_UNILAB.png" alt="">
-				<div class="duas colunas no-meio borda fundo-branco">					
-			            <div class="no-centro">
-			                <h1>Catraca</h1>
-			            </div>
-			            <div class="linha com-bordas alinhado-a-esquerda">
-			                <div class="conteudo"><form method="post" action="" class="formulario-organizado">
-									
-			                       <label for="idTextLogin">
-			                           Login
-			                           <input type="text" name="login" id="idTextLogin" class="doze" placeholder="Digite seu Usuário"/>
-			                        </label>
-			                        <label for="idTextSenha">
-			                            Senha
-			                            <input type="password" name="senha" id="idTextSenha" class="doze" placeholder="Digite sua Senha" />
-			                        </label>
-			                       <button type="submit" name="formlogin" class="botao b-primario doze"><span class="icone-redo2"></span> Entrar </button>                
-			                    </form>
-			                    <a href="http://sigadmin.unilab.edu.br/admin/public/recuperar_login.jsf" class="medio centralizado doze colunas">Não consegue acessar o sistema?</a>                 
-			                </div>
-			            </div>		            
-			     	</div>
-					
 			</div>
-			<div id="rodape" class="doze colunas fundo-azul1 centralizado rodape">
+					
+			<div class="doze colunas">				
+				<div class="resolucao config">
+					<div class="duas colunas">					
+						<div class="padding">
+						    <a href="#expandir_menu" title="Clique para expandir o menu" class="menu-resp icone-menu2"> Menu vertical</a>
+						    <div id="expandir_menu" class="menu-vertical">
+						        <a href="#ocultar_menu" class="fechar-menu icone-cross"></a>
+						        <ol>
+						            <li><a href="" class="item-vertical-ativo"><span class="icone-home3"></span> <span class="item-vertical-texto">Início</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-cogs"></span> <span class="item-vertical-texto">Definições</span></a></li>						            					            
+						            <li><a href="" class="item-vertical"><span class="icone-loop2"></span> <span class="item-vertical-texto">Catraca</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-profile"></span> <span class="item-vertical-texto">Cartão</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-user"></span> <span class="item-vertical-texto">Guichê</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-file-text2"></span> <span class="item-vertical-texto">Relatório</span></a></li>
+						         	<li><a href="" class="item-vertical"><span class="icone-exit"></span> <span class="item-vertical-texto">Sair</span></a></li>
+						        </ol>
+						    </div>
+						</div>
+					</div>
+					<div class="dez colunas">					
+						
+					Conteudo
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<footer>
+			<div id="rodape" class="doze colunas fundo-azul1 centralizado">
 				<p>CATRACA todos os direitos reservados</p>
-			</div>			
-		</div>		
+			</div>
+		</footer>	
 	</body>
 </html>
