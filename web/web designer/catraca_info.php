7b3d055290e4e5035800b74001e67102759dfef4
Alan Cleber Morais Gomes <kleber.acmg@gmail.com>
2015-11-30 16:59:04 -0300

    Pagina catraca_info.

---
+++ web/web designer/catraca_info.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+
+		<meta charset="UTF-8">
+		<meta name="description" content="Curso Bootstrap - PontoCanal"/>
+		<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+		
+		<title>Projeto Catraca</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<link rel="stylesheet" href="http://spa.dsi.unilab.edu.br/spa/css/spa.css" />		
+		<link rel="stylesheet" href="css/estilo.css" type="text/css" media="screen">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>		
+		<script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>
+		<script src="js/bootstrap.js"></script>		
+
+	</head>
+
+	<body >
+		<div class="pagina fundo-cinza1">
+			<div id="barra-governo">
+			    <div class="resolucao config">
+			       <div class="a-esquerda">
+			          <a href="http://brasil.gov.br/" target="_blank"><span id="bandeira"></span><span>BRASIL</span></a>
+			          <a href="http://acessoainformacao.unilab.edu.br/" target="_blank">Acesso à informação</a>
+			       </div>
+			       <div class="a-direita"><a href="#"><i class="icone-menu"></i></a></div>
+			       <ul>
+			          <li><a href="http://brasil.gov.br/barra#participe" target="_blank">Participe</a></li>
+			          <li><a href="http://www.servicos.gov.br/" target="_blank">Serviços</a></li>
+			          <li><a href="http://www.planalto.gov.br/legislacao" target="_blank">Legislação</a></li>
+			          <li><a href="http://brasil.gov.br/barra#orgaos-atuacao-canais" target="_blank">Canais</a></li>
+			       </ul>
+			    </div>
+			</div>
+
+			<div class="doze colunas banner gradiente">			
+
+				<div id="topo" class="resolucao config">
+					<div class="tres colunas">
+						<a href="http://www.dti.unilab.edu.br"><img class="imagem-responsiva" src="img/logo_h-site.png" alt=""></a>				
+					</div>
+					<div class="seis colunas centralizado">
+						<h1>CATRACA<br><small class="texto-branco">Controle Administrativo de Tráfego Acadêmico Automatizado</small></h1>
+					</div>
+					<div class="tres colunas alinhado-a-direita">
+						<a href="http://www.unilab.edu.br"><img class="imagem-responsiva centralizada" src="img/logo-unilab-branco.png" alt=""></a>
+					</div>			
+				</div>
+			</div>
+
+			<div id="barra" class="doze colunas fundo-azul3 alinhado-a-direita">
+				<div class="config">
+					<a href="#"><span>Perguntas frequentes |</span></a>
+					<a href="#"><span>Contato |</span></a>
+					<a href="#"><span>Serviços |</span></a>
+					<a href="#"><span>Dados Abertos |</span></a>
+					<a href="#"><span>Área de Imprensa |</span></a>
+				</div>
+			</div>		
+						
+			<div class="doze colunas">
+				<div class="resolucao config">
+					<div class="duas colunas">					
+						<div class="padding">
+						    <a href="#expandir_menu" title="Clique para expandir o menu" class="menu-resp icone-menu2"> Menu vertical</a>
+						    <div id="expandir_menu" class="menu-vertical">
+						        <a href="#ocultar_menu" class="fechar-menu icone-cross"></a>
+						        <ol>
+						            <li><a href="" class="item-vertical-ativo"><span class="icone-home3"></span> <span class="item-vertical-texto">Início</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-cogs"></span> <span class="item-vertical-texto">Definições</span></a></li>						            					            
+						            <li><a href="" class="item-vertical"><span class="icone-loop2"></span> <span class="item-vertical-texto">Catraca</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-credit-card"></span> <span class="item-vertical-texto">Cartão</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-user"></span> <span class="item-vertical-texto">Guichê</span></a></li>
+						            <li><a href="" class="item-vertical"><span class="icone-file-text2"></span> <span class="item-vertical-texto">Relatório</span></a></li>
+						         	<li><a href="" class="item-vertical"><span class="icone-exit"></span> <span class="item-vertical-texto">Sair</span></a></li>
+						        </ol>
+						    </div>
+						</div>
+					</div>
+					<div class="dez colunas">
+						<div class="doze colunas">
+							<div id="titulo" class="doze colunas fundo-azul2 centralizado ">
+								<h1 class="texto-branco">Visualização de Catracas</h1>
+							</div>						
+							<div class="oito coluna no-meio borda catraca">
+								<h3 class="centralizado negrito">RULIBERDADE-01</h3>
+								<hr class="solida" />
+								<div class="catraca-icone fill-verde">									
+									<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+									 width="100pt" height="110pt" viewBox="0 0 903.000000 1000.000000"
+									 preserveAspectRatio="xMidYMid meet">									
+										<g transform="translate(0.000000,1000.000000) scale(0.100000,-0.100000)" stroke="none">
+											<path d="M1648 9523 c-9 -10 -102 -148 -208 -308 l-193 -290 1 -90 c4 -285 54
+											-2565 58 -2592 l5 -31 101 -8 101 -7 -6 -481 c-4 -265 -14 -1383 -23 -2486
+											-13 -1530 -20 -2007 -29 -2015 -6 -5 -37 -125 -68 -265 -58 -267 -59 -285 -14
+											-295 12 -3 290 -21 618 -40 407 -24 605 -32 622 -26 14 6 28 18 31 28 6 18 83
+											651 128 1051 22 191 22 192 3 211 -18 18 -49 22 -280 35 -442 27 -466 29 -469
+											35 -2 3 0 110 5 236 4 127 13 392 18 590 6 198 24 760 41 1250 16 490 34 1061
+											40 1270 6 209 15 493 20 630 5 138 10 280 10 317 l0 67 185 156 c185 155 186
+											156 208 138 20 -17 78 -77 209 -217 l45 -49 67 1 c60 1 66 -1 71 -21 6 -23 69
+											-407 180 -1092 95 -585 116 -714 185 -1135 105 -643 168 -1030 236 -1445 35
+											-220 66 -411 69 -424 13 -72 67 -76 288 -25 71 17 106 47 93 79 -3 8 -125 734
+											-271 1612 -146 879 -277 1663 -290 1743 -76 439 -91 541 -84 546 5 3 18 -14
+											28 -38 85 -197 378 -861 419 -950 39 -85 75 -112 144 -104 56 6 94 28 114 68
+											21 41 26 70 15 93 -6 11 -40 92 -76 180 -36 88 -79 190 -96 226 -16 36 -29 69
+											-29 73 0 4 -13 35 -29 69 -16 34 -34 79 -42 99 -9 27 -16 34 -22 25 -6 -8 -7
+											-7 -3 5 5 16 -4 44 -39 123 -8 17 -32 75 -55 130 -23 55 -48 114 -55 130 -19
+											42 -38 88 -40 100 -2 6 -17 40 -34 78 -43 93 -41 107 18 152 118 90 307 307
+											422 483 26 41 38 52 54 48 40 -10 161 -27 900 -131 413 -58 953 -135 1200
+											-170 248 -35 788 -112 1200 -170 413 -58 799 -112 859 -121 128 -18 154 -12
+											181 44 40 84 48 242 14 277 -9 9 -91 22 -589 94 -113 17 -326 48 -475 71 -148
+											22 -346 51 -440 65 -93 14 -294 43 -445 65 -151 23 -349 52 -440 65 -91 13
+											-289 43 -440 65 -441 66 -816 121 -1327 196 -209 31 -263 42 -263 53 0 9 -21
+											36 -46 62 l-46 47 15 44 c9 23 21 102 27 173 6 72 22 229 35 350 13 121 26
+											261 29 310 3 50 8 135 12 190 l6 100 -73 85 c-151 175 -374 425 -388 437 -9 6
+											-47 14 -86 17 -38 3 -227 20 -420 36 -192 16 -534 46 -760 65 -225 19 -441 37
+											-480 40 -38 3 -86 8 -105 11 -27 4 -40 0 -52 -13z m2727 -2194 c39 -5 86 -14
+											105 -19 19 -6 61 -10 92 -10 31 0 69 -5 85 -11 15 -6 63 -12 105 -15 43 -2 86
+											-7 95 -13 10 -5 57 -12 104 -15 47 -3 107 -12 135 -20 27 -8 89 -16 137 -20
+											49 -3 94 -9 100 -14 7 -5 55 -12 107 -15 52 -3 124 -12 160 -21 62 -14 203
+											-34 300 -42 25 -2 74 -8 110 -13 36 -6 133 -19 215 -30 83 -11 193 -27 245
+											-35 52 -9 131 -20 175 -26 83 -11 182 -26 395 -59 143 -22 265 -39 330 -46 25
+											-3 68 -9 95 -15 28 -5 124 -18 215 -30 91 -11 199 -24 240 -30 41 -5 154 -19
+											250 -31 96 -11 178 -23 182 -25 5 -3 13 5 18 18 8 17 9 10 5 -32 -4 -30 -8
+											-56 -9 -58 -3 -2 -225 28 -556 78 -394 58 -632 93 -785 115 -93 14 -213 32
+											-265 40 -104 16 -398 60 -665 100 -93 14 -210 32 -260 40 -49 8 -166 26 -260
+											40 -93 14 -228 34 -300 44 -71 11 -193 29 -270 41 -77 12 -149 23 -160 26 -11
+											2 -67 11 -125 19 -58 8 -150 22 -205 30 -55 9 -129 20 -165 25 -97 13 -169 30
+											-176 41 -3 6 4 7 19 4 14 -4 28 -2 31 3 4 7 19 6 43 0 21 -5 70 -14 108 -19z
+											m-1147 -865 c18 -8 32 -22 32 -30 0 -8 5 -14 11 -14 6 0 8 -9 4 -21 -5 -13 -3
+											-19 3 -15 11 6 13 -4 36 -159 8 -55 20 -124 26 -154 5 -29 10 -71 10 -92 0
+											-22 5 -39 11 -39 6 0 8 -8 4 -19 -3 -11 -1 -22 5 -26 6 -3 8 -11 5 -16 -3 -5
+											-1 -35 5 -66 12 -60 14 -72 24 -138 4 -22 11 -51 16 -65 4 -14 11 -59 13 -101
+											3 -41 12 -104 20 -140 13 -54 28 -146 42 -249 11 -85 26 -173 30 -180 4 -7 26
+											-144 61 -385 3 -22 9 -58 14 -80 5 -22 10 -58 10 -81 0 -22 4 -44 9 -50 5 -5
+											11 -34 14 -64 5 -52 19 -144 103 -680 79 -506 192 -1192 205 -1253 6 -26 8
+											-55 3 -65 -4 -9 -6 -23 -5 -29 2 -15 -35 -17 -44 -3 -3 6 -14 10 -23 10 -13 0
+											-17 9 -18 38 -1 20 -11 96 -23 167 -12 72 -31 189 -42 260 -26 158 -27 166
+											-72 435 -69 410 -112 671 -141 850 -8 52 -22 136 -31 185 -9 50 -20 118 -26
+											152 -5 34 -22 144 -39 245 -17 101 -37 221 -44 268 -8 47 -26 150 -40 230 -14
+											80 -34 204 -45 275 -37 240 -97 595 -111 655 -5 19 -16 78 -25 130 -9 52 -23
+											133 -32 179 -8 47 -16 90 -17 98 -1 7 -5 16 -9 20 -6 5 18 25 39 33 0 0 15 -7
+											32 -16z"/>
+										</g>
+									</svg>
+								</div>
+								<div class="catraca-info">
+									<span>Status: Ativo</span><br>
+									<span>Inicio do Turno: 11:00</span><br>
+									<span>Fim do Turno: 13:50</span><br>
+									<span>Giros Por Turno: 00</span><br>
+									<span>Total de Giros: 00</span><br>
+									<hr class="solida" />
+									<span>Unidade Acadêmica: Liberdade</span><br>
+									<span>IP: 192.168.0.1</span>						
+								</div>
+								<div class="doze colunas centralizado">
+									<hr class="solida" />
+									<a href="" class="botao texto-branco">Adicionar Unidade Acadêmica</a>
+								</div>														
+							</div>
+						</div>
+
+						<div id="formcatraca" class="oito colunas no-meio borda">												
+							<form method="post" action="" class="formulario sequencial" >											
+								<label for="opcoes">
+									<object class="rotulo">Unidade Acadêmica: </object>
+									<select name="opcoes" id="opcoes">
+										<option value="1">Liberdade</option>											
+									</select>																							    
+								</label>
+								<input class="botao" type="submit" name="adicionar" id="adicionar" />
+							</form>							
+						</div>
+
+					</div>					
+				</div>
+			</div>			
+		</div>
+		<footer id="rodape" class="doze colunas fundo-azul1 centralizado">
+			<p>CATRACA todos os direitos reservados</p>
+		</footer>		
+	</body>
+</html>
