45063630b7280ba8c6df0c9c39adb82f711dd74c
Jefferson Uchoa Ponte <jefponte@gmail.com>
2016-04-12 10:22:17 -0300

    Adicionando codigo do Identificador

---
+++ web/identificador/classes/util/Auditoria.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * 
+ * @author Jefferson Uchoa Ponte
+ * 
+ * Como usar
+ * Para começar a usar chame o metodo estático da seguinte forma: 
+ * 
+ * Auditoria::criarTabela(new PDO("pgsql:host=localhost dbname=nome_do_banco user=usuario_do_banco password=senhaDoBanco"));
+ * 
+ * Após a tabela criada
+ * Inclua a classe auditoria no arquivo PHP da pagina que deseja auditar. 
+ * 
+ * $auditoria1 = new Auditoria(new PDO("pgsql:host=localhost dbname=nome_do_banco user=usuario_do_banco password=senhaDoBanco"));
+ * 
+ * $auditoria1->cadastrar(12);//Ou $auditoria1->cadastrar(12, "Alterando tabela endereco");
+ */
+class Auditoria{
+	private $conexao;
+	public function Auditoria(PDO $conexao = NULL){
+		if ($conexao != null) 
+		{
+			$this->conexao = $conexao;
+		}
+		else 
+		{
+			$this->conexao = new PDO ( "pgsql:host=localhost dbname=nome_do_banco user=usuario_do_banco password=senhaDoBanco" );
+		}
+	}
+	public function cadastrar($idDoUsuario, $obs = "-"){
+		$url = $_SERVER['REQUEST_URI'];
+		$idDoUsuario = intval($idDoUsuario);
+		$data = date("Y-m-d H:i:s");
+		$sql = "INSERT INTO auditoria(audi_pagina, audi_data, usua_id, audi_observacao) VALUES('$url', '$data', $idDoUsuario, '$obs');";
+		$this->conexao->exec($sql);
+	}
+	/**
+	 * Caso queira criar a tabela no banco local passe uma conexão PDO. 
+	 * @param PDO $conexao
+	 */
+	public static function criarTabela(PDO $conexao){
+		$conexao->exec("CREATE TABLE auditoria(audi_id serial NOT NULL,
+				audi_pagina character varying(200),audi_data timestamp without time zone,  
+				usua_id integer,  audi_observacao character varying(200),  CONSTRAINT auditoria_pkey PRIMARY KEY (audi_id));");		
+	}
+}
+
+
+
+
+?>
---
+++ web/identificador/teste.php
@@ -0,0 +1,41 @@
+
+<?php
+ini_set ( 'display_errors', 1 );
+ini_set ( 'display_startup_erros', 1 );
+error_reporting ( E_ALL );
+
+date_default_timezone_set ( 'America/Araguaina' );
+
+function __autoload($classe) {
+	if (file_exists ( 'classes/dao/' . $classe . '.php' ))
+		include_once 'classes/dao/' . $classe . '.php';
+		if (file_exists ( 'classes/model/' . $classe . '.php' ))
+			include_once 'classes/model/' . $classe . '.php';
+			if (file_exists ( 'classes/controller/' . $classe . '.php' ))
+				include_once 'classes/controller/' . $classe . '.php';
+				if (file_exists ( 'classes/util/' . $classe . '.php' ))
+					include_once 'classes/util/' . $classe . '.php';
+					if (file_exists ( 'classes/view/' . $classe . '.php' ))
+						include_once 'classes/view/' . $classe . '.php';
+}
+
+
+
+$dao = new DAO();
+
+$sql = "UPDATE usuario set usua_nivel = 3 WHERE usua_login = 'jefponte';";
+echo $dao->getConexao()->exec($sql);
+
+// $sql = "DELETE FROM vinculo_tipo";
+// $sql = "DELETE FROM vinculo WHERE vinc_avulso <> 'TRUE'";
+//$sql = "INSERT INTO vinculo_tipo (vinc_id, tipo_id) VALUES(8, 13)";
+// $sql = "DELETE FROM catraca WHERE catr_id > 5";
+
+// $sql = "UPDATE turno set turn_hora_fim = '13:30:00'
+// 		WHERE turn_id = 1";
+
+// $i = $result = $dao->getConexao()->exec($sql);
+// echo $i;
+
+
+?>
