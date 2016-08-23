e7a8625044b05b062522f7641c83611addadfd48
Jefferson Uchoa Ponte <jefponte@gmail.com>
2016-08-23 11:58:36 -0300

    Mudanças UnidadeDao Alan

--- web/identificador/classes/dao/UnidadeDAO.php
+++ web/identificador/classes/dao/UnidadeDAO.php
@@ -1,261 +1,199 @@
 <?php
-/**
- * UnidadeDAO alteraï¿½ï¿½es do banco de dados referentes ï¿½ entidade unidade. 
- * Gera pesistencia da classe unidade. 
- * ï¿½ a unidade academica. 
- * @author jefponte
- *
- */
-class UnidadeDAO extends DAO {
+class UsuarioDAO extends DAO {
+	
+	
 	
 	/**
-	 * Retorna Um vetor de unidades.
-	 *
-	 * @return multitype:Unidade
+	 * Vamos verificar dois bancos. 
+	 * Primeiro no Banco Local. Se ele n�o existir olhamos no SIG. 
+	 * Se existir no SIG copiamos para o local com n�vel Default. 
+	 * @param Usuario $usuario
+	 * @return boolean
 	 */
-	public function retornaLista() {
-		$lista = array ();
-		$result = $this->getConexao ()->query ( "SELECT * FROM unidade LIMIT 100" );
+	public function autentica(Usuario $usuario) {
 		
-		foreach ( $result as $linha ) {
-			$unidade = new Unidade ();
-			$unidade->setId ( $linha ['unid_id'] );
-			$unidade->setNome ( $linha ['unid_nome'] );
-			$lista [] = $unidade;
-		}
-		return $lista;
-	}
-	public function inserirUnidade(Unidade $unidade) {
-		$nome = $unidade->getNome ();
-		if ($this->getConexao ()->query ( "INSERT INTO unidade (unid_nome) VALUES('$nome')" ))
-			return true;
-		return false;
-	}
-	public function deletarUnidade(Unidade $unidade) {
-		$id = $unidade->getId ();
-		$sql = "DELETE FROM unidade WHERE unid_id = $id";
-		if ($this->getConexao ()->query ( $sql ))
-			return true;
-		return false;
-	}
-
-	public function retornaCatracasPorUnidade(Unidade $unidade = null) {
-		$lista = array ();
-		if ($unidade != null) {
-			$id = $unidade->getId ();
-			$sql = "SELECT 
-			
-					*, catraca.catr_id as catraca_id
-					 
-					FROM catraca 
-					INNER JOIN catraca_unidade
-					ON catraca.catr_id = catraca_unidade.catr_id
-					INNER JOIN unidade
-					ON unidade.unid_id = catraca_unidade.unid_id
-					WHERE unidade.unid_id = $id";
-		} else {
-			$sql = "SELECT *, catraca.catr_id as 
-					catraca_id
-					 FROM catraca
-					LEFT JOIN catraca_unidade
-					ON catraca.catr_id = catraca_unidade.catr_id
-					LEFT JOIN unidade
-					ON unidade.unid_id = catraca_unidade.unid_id
-					ORDER BY catraca.catr_id ASC
-					
-					";
-		}
+		/*
+		 * Primeiro vou verificar no banco local . 
+		 * Deu certo?
+		 * Define nivel na session e deixa o cara logado. 
+		 * 
+		 */
+		
+		
+		$login = $usuario->getLogin ();
+		$senha = md5 ( $usuario->getSenha () );
+		$sql = "SELECT * FROM usuario WHERE usua_login ='$login' AND usua_senha = '$senha' LIMIT 1";
 		
 		foreach ( $this->getConexao ()->query ( $sql ) as $linha ) {
-			$catraca = new Catraca ();
-			$catraca->setNome ( $linha ['catr_nome'] );
-			$catraca->setOperacao($linha['catr_operacao']);
-			$catraca->setTempoDeGiro($linha['catr_tempo_giro']);
-			$catraca->setIp($linha['catr_ip']);
-			$catraca->setId($linha['catraca_id']);
-			$catraca->setUnidade(new Unidade());
-			$catraca->getUnidade()->setNome($linha['unid_nome']);
-			$catraca->setFinanceiro($linha['catr_financeiro']);
-			
-			$lista [] = $catraca;
+			$usuario->setLogin ( $linha ['usua_login'] );
+			$usuario->setId ( $linha ['usua_id'] );
+			$usuario->setNivelAcesso ( $linha ['usua_nivel'] );
+			return true;
 		}
+		//N�o deu. 
+		//Vou verificar na base do SIG. 
+		$daoSistemasComum = new DAO(null, DAO::TIPO_PG_SISTEMAS_COMUM);
+		$result2 = 	$daoSistemasComum->getConexao()->query("SELECT * FROM vw_usuarios_autenticacao_catraca WHERE login ='$login' AND senha = '$senha' LIMIT 1");
+		foreach($result2 as $linha){
+			
+			//Se eu to procurando aqui � pq houve algo errado no banco local. 
+			//2 n�o tinha. -- nesse caso fazemos um insert. 
+			//Vamos verificar isso agora. 
+			//Existe esse login?
+			
+			//1 Minha senha est� desatualizada no local. -- Nesse caso fazemos update na senha e tentamos autenticar de novo com o Nivel que tenho.
+			$result3 = $this->getConexao()->query("SELECT * FROM usuario WHERE usua_login = '$login' LIMIT 1");
+			foreach($result3 as $outraLinha){
+				//Vamos atualizar sua senha, meu filho. 
+				$this->getConexao()->query("UPDATE usuario set usua_senha = '$senha' WHERE usua_login = '$login'");
+				//Caso isso aconteceu, podemos logar de novo. Mesmo augoritimo de antes.  Fa�amos recursividade? N�o, � meio arriscado, Vamos repetir mesmo. 
+				foreach ( $this->getConexao ()->query ( $sql ) as $linha2 ) {
+					$usuario->setLogin ( $linha2 ['usua_login'] );
+					$usuario->setId ( $linha2 ['usua_id'] );
+					$usuario->setNivelAcesso ( $linha2 ['usua_nivel'] );
+					return true;
+				}
+				
+			}
+			//Vish, o cara n�o existia na base local. O que faremos? 
+			//Num tem pobrema! Nois adiciona! N�is rai farr� um incerte. 		
+			$nivel = Sessao::NIVEL_COMUM;
+			$nome = $linha['nome'];
+			$email = $linha['email'];
+			$idBaseExterna = $linha['id_usuario'];
+			$this->getConexao()->query("INSERT into usuario(usua_login,usua_senha, usua_nome,usua_email, usua_nivel, id_base_externa) 
+										VALUES				('$login', '$senha', '$nome','$email', $nivel, $idBaseExterna)");
+			$usuario->setNivelAcesso ( $nivel);
+			return true;
+			
+		}
+		 
+		
+		
+		return false;
+	}
+
+	public function pesquisaNoSigaa($pesquisa){
+		$lista = array();
+		$pesquisa = preg_replace ('/[^a-zA-Z0-9\s]/', '', $pesquisa );
+		$pesquisa = strtoupper ( $pesquisa );
+		$sql = "SELECT * FROM vw_usuarios_catraca WHERE nome LIKE '%$pesquisa%' LIMIT 30";
+		foreach($this->getConexao()->query($sql) as $linha){
+			$usuario = new Usuario();
+			$usuario->setNome($linha['nome']);
+			$usuario->setEmail($linha['email']);
+			$usuario->setLogin($linha['login']);
+			$usuario->setIdBaseExterna($linha['id_usuario']);
+			$usuario->setCpf($linha['cpf_cnpj']);
+			$usuario->setIdentidade($linha['identidade']);
+			$usuario->setPassaporte($linha['passaporte']);
+			$usuario->setSiape($linha['siape']);
+			$usuario->setTipoDeUsuario($linha['tipo_usuario']);
+			$usuario->setMatricula($linha['matricula_disc']);
+			$usuario->setStatusDiscente($linha['status_discente']);
+			$usuario->setNivelDiscente($linha['nivel_discente']);
+			$usuario->setCategoria($linha['categoria']);
+			$usuario->setStatusServidor($linha['status_servidor']);
+			$lista[] = $usuario;
+		}
+		
 		return $lista;
-	}
-
-	public function totalDeGirosDaCatraca(Catraca $catraca){
-		$idCatraca = $catraca->getId();
-		$filtro = "";
 		
-		$sql = "SELECT sum(1) as resultado FROM registro INNER JOIN catraca ON registro.catr_id = catraca.catr_id
-				WHERE catraca.catr_id = $idCatraca $filtro";
-		
-		$resultado = 0;
-		foreach ($this->getConexao()->query($sql) as $linha){
-			$resultado = $linha['resultado'];
-		}
-		return $resultado;
 	}
-	
-	public function totalDeGirosDaCatracaTurnoAtual(Catraca $catraca, Tipo $tipo = null){
-		$resultado = 0;
-		$idCatraca = $catraca->getId();
-		$turno = $this->pegaTurnoAtualSeExistir();
-		if($turno){
-			$horaInicial = date("Y-m-d ").$turno->getHoraInicial();
-			$horaFinal = date("Y-m-d ").$turno->getHoraFinal();
-			if($tipo == null){
-				
-				$sql = "SELECT sum(1) as resultado FROM registro INNER JOIN catraca ON registro.catr_id = catraca.catr_id
-			WHERE catraca.catr_id = $idCatraca AND registro.regi_data BETWEEN '$horaInicial' AND '$horaFinal' ";
-				
+	public function retornaPorIdBaseExterna(Usuario $usuario){
+		$id = $usuario->getIdBaseExterna();
+		$sql = "SELECT * FROM vw_usuarios_catraca WHERE id_usuario = $id LIMIT 1";
+		foreach ($this->getConexao ()->query ( $sql ) as $linha){
+			$usuario->setNome($linha['nome']);
+			$usuario->setEmail($linha['email']);
+			$usuario->setLogin($linha['login']);
+			$usuario->setCpf($linha['cpf_cnpj']);
+			$usuario->setIdBaseExterna($linha['id_usuario']);
+			$usuario->setIdentidade($linha['identidade']);
+			$usuario->setPassaporte($linha['passaporte']);
+			$usuario->setTipoDeUsuario($linha['tipo_usuario']);
+			$usuario->setMatricula($linha['matricula_disc']);
+			$usuario->setStatusDiscente($linha['status_discente']);
+			$usuario->setNivelDiscente($linha['nivel_discente']);
+			$usuario->setCategoria($linha['categoria']);
+			$usuario->setSiape($linha['siape']);
+			$usuario->setStatusServidor($linha['status_servidor']);
+			return $usuario;
 			
-			}
-			else{
-				$idTipo = $tipo->getId();
-				$sql = "SELECT sum(1) as resultado FROM registro INNER JOIN catraca ON registro.catr_id = catraca.catr_id
-				INNER JOIN cartao ON cartao.cart_id = registro.cart_id
-				WHERE catraca.catr_id = $idCatraca AND registro.regi_data BETWEEN '$horaInicial' AND '$horaFinal' AND cartao.tipo_id = $idTipo";
-				
-			}
-
-			foreach ($this->getConexao()->query($sql) as $linha){
-				$resultado = $linha['resultado'];
-			}
-			
-		}
-		
-		
-		return $resultado;
-	}
-	public function pegaTurnoAtualSeExistir(){
-		$dataTimeAtual = date ( "G:i:s" );
-		$sql = "Select * FROM turno WHERE '$dataTimeAtual' BETWEEN turno.turn_hora_inicio AND turno.turn_hora_fim";
-		$result = $this->conexao->query($sql);
-		$turno = null;
-		foreach ($result as $linha){
-			$turno = new Turno();
-			$turno->setHoraInicial($linha['turn_hora_inicio']);
-			$turno->setHoraFinal($linha['turn_hora_fim']);
-			
-		}
-		return $turno;
-	}
-	public function preencheCatracaPorId(Catraca $catraca){
-		$idCatraca = $catraca->getId();
-		$sql = "
-				SELECT *, catraca.catr_id as catraca_id FROM catraca
-		LEFT JOIN catraca_unidade
-		ON catraca.catr_id = catraca_unidade.catr_id
-		LEFT JOIN unidade
-		ON unidade.unid_id = catraca_unidade.unid_id
-		WHERE catraca.catr_id = $idCatraca";
-		foreach ( $this->getConexao ()->query ( $sql ) as $linha ) {
-			$catraca->setNome ( $linha ['catr_nome'] );
-			$catraca->setId ( $linha ['catraca_id'] );
-			$catraca->setOperacao($linha['catr_operacao']);
-			$catraca->setTempoDeGiro($linha['catr_tempo_giro']);
-			$catraca->setIp($linha['catr_ip']);
-			$catraca->setFinanceiro($linha['catr_financeiro']);
-			$catraca->setUnidade(new Unidade());
-			$catraca->getUnidade()->setNome($linha['unid_nome']);
-				
 		}
 		
 	}
 	/**
+	 * Pesquisaremos primeiro o Login do usuario e depois o nome do laboratorio.
+	 * Apos isso pegaremos o Id de cada um e usaremos numa operacao de insert.
+	 * Mas essa operacao so pode funcionar se ela ainda nao existir com esse usuario e laboratorio.
+	 * Terminando tudo iremos atualizar o nivel do usuario
 	 * 
-	 * @param Catraca $catraca
+	 * @param Usuario $usuario        	
+	 * @param Laboratorio $laboratorio        	
 	 */
-	public function atualizarCatraca(Catraca $catraca){
-		$giro = $catraca->getTempodeGiro();
-		$id = $catraca->getId();
-		$operacao = $catraca->getOperacao();
-		$idUnidade= $catraca->getUnidade()->getId();
+	public function tornarAdministrador(Usuario $usuario, Laboratorio $laboratorio) {
+		$login = $usuario->getLogin();
+		$nomeLaboratorio = $laboratorio->getNome();
 		
 		
+		$sqlLaboratorio = "SELECT * FROM laboratorio WHERE nome_laboratorio = $nomeLaboratorio";
+		
+		
+		
+	}
+	public function preenchePorLogin(Usuario $usuario){
+		
+		$login = $usuario->getLogin();
+		$login = preg_replace ('/[^a-zA-Z0-9\s]/', '', $login);
+		$sql = "SELECT * FROM usuario WHERE usua_login = '$login'";
+		foreach($this->getConexao()->query($sql) as $linha){
+			$usuario->setId($linha['usua_id']);
+			$usuario->setNome($linha['usua_nome']);
+			return true;
+		}
+		return false;
+		
+		
+	}
+	public function preenchePorNome(Laboratorio $laboratorio){
+		$nome = $laboratorio->getNome();
+		$sql = "SELECT * FROM laboratorio WHERE nome_laboratorio = '$nome'";
+		foreach($this->getConexao()->query($sql) as $linha){
+			$laboratorio->setId($linha['id_laboratorio']);
+			return true;
+		}
+		return false;
+	}
+	public function ehAdministrador(Usuario $usuario, Laboratorio $laboratorio){
+		$idUsuario = $usuario->getId();
+		$idLaboratorio = $laboratorio->getId();
+		$sql= "SELECT * FROM administrador WHERE id_usuario = $idUsuario AND id_laboratorio = $idLaboratorio";
+		$result =$this->getConexao()->query($sql);
+		foreach($result as $linha){
+			return true;
+		}
+		return false;
+	}
+	public function adicionaAdministrador(Usuario $usuario, Laboratorio $laboratorio){
+		$novoNivel = Sessao::NIVEL_ADMIN;
+		$idUsuario = $usuario->getId();
+		$idLaboratorio = $laboratorio->getId();
+		
+		$sqlUpdate = "UPDATE usuario set nivel_acesso = $novoNivel WHERE id_usuario = $idUsuario";
+		$sqlInsert = "INSERT into administrador (id_usuario, id_laboratorio) VALUES($idUsuario, $idLaboratorio)";
 		$this->getConexao()->beginTransaction();
-		$sqlUpdate = "UPDATE catraca SET catr_tempo_giro = $giro, 
-						catr_operacao = $operacao
-						WHERE catr_id = $id";
-		if(!$this->getConexao()->exec($sqlUpdate))
-		{
-			$this->getConexao()->rollBack();
-			echo $sqlUpdate;
-			return false;
-		}		
-		$this->getConexao()->exec("DELETE FROM catraca_unidade WHERE catr_id = $id");
-		if(!$this->getConexao()->exec("INSERT into catraca_unidade(catr_id, unid_id) VALUES($id, $idUnidade)"))
-		{
-			$this->getConexao()->rollBack();
-
-			return false;
+		if($this->getConexao()->query($sqlUpdate)){
+			if($this->getConexao()->query($sqlInsert)){
+				$this->getConexao()->commit();
+				return true;
+			}
 		}
-		$this->getConexao()->commit();
-		return true;
-		
-		
-		
-	}
-	public function preenchePorId(Unidade $unidade) {
-		$idUnidade = $unidade->getId ();
-		if (! is_int ( $idUnidade ) && $idUnidade <= 0)
-			return;
-		$sql = "SELECT * FROM unidade WHERE unid_id = $idUnidade";
-		foreach ( $this->getConexao ()->query ( $sql ) as $linha )
-			$unidade->setNome ( $linha ['unid_nome'] );
-		return;
-	}
-	public function turnoNaUnidade(Turno $turno, Unidade $unidade) {
-		$idTurno = $turno->getId ();
-		$idUnidade = $unidade->getId ();
-		if ($idUnidade <= 0 || $idTurno <= 0 || ! is_int ( $idUnidade ) || ! is_int ( $idTurno ))
-			return;
-			// Primeiro vamos ver se esse turno existe nessa unidade.
-		
-		$sqlExiste = "SELECT * FROM unidade_turno WHERE turn_id = $idTurno AND unid_id = $idUnidade";
-		foreach ( $this->getConexao ()->query ( $sqlExiste ) as $linha )
-			return false;
-		
-		$sqlInsert = "INSERT INTO unidade_turno(turn_id, unid_id) VALUES($idTurno, $idUnidade)";
-		if ($this->getConexao ()->exec ( $sqlInsert ))
-			return true;
+		$this->getConexao()->rollBack();
 		return false;
 		
-		// Caso contrÃ¡rio a gente insere.
 	}
 	
-	public function excluirTurnoDaUnidade(Turno $turno, Unidade $unidade) {
-		$idTurno = $turno->getId ();
-		$idUnidade = $unidade->getId ();
-		if ($idUnidade <= 0 || $idTurno <= 0 || ! is_int ( $idUnidade ) || ! is_int ( $idTurno ))
-			return;
-		$sqlInsert = "DELETE FROM unidade_turno WHERE unid_id = $idUnidade AND  turn_id = $idTurno";
-		if ($this->getConexao ()->exec ( $sqlInsert ))
-			return true;
-		return false;
-	
-		// Caso contrÃ¡rio a gente insere.
-	}
-	
-	public function turnosDaUnidade(Unidade $unidade) {
-		$idUnidade = $unidade->getId ();
-		$sql = "SELECT * FROM unidade 
-				INNER JOIN 
-				unidade_turno ON unidade.unid_id = unidade_turno.unid_id 
-				INNER JOIN turno 
-				ON turno.turn_id = unidade_turno.turn_id
-				WHERE unidade.unid_id = $idUnidade";
-		foreach ( $this->getConexao ()->query ( $sql ) as $linha ) {
-			
-			$turno = new Turno ();
-			$turno->setId ( $linha ['turn_id'] );
-			$turno->setDescricao ( $linha ['turn_descricao'] );
-			$turno->setHoraFinal ( $linha ['turn_hora_fim'] );
-			$turno->setHoraInicial ( $linha ['turn_hora_inicio'] );
-			$unidade->adicionaTurno ( $turno );
-		}
-	}
-
 }
 
 ?>
