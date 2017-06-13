4845d1d295b1e5f49b661014ca6ec0d56b8ddd06
Jefferson Uchoa Ponte <jefponte@gmail.com>
2017-06-13 00:10:50 -0300

    Cliente e servidor para troca de informações e atualização.

--- catraca_virtual/src/main/java/br/edu/unilab/catraca/verificador_de_conexao/Cliente.java
+++ catraca_virtual/src/main/java/br/edu/unilab/catraca/verificador_de_conexao/Cliente.java
@@ -1,19 +1,34 @@
 package br.edu.unilab.catraca.verificador_de_conexao;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Cliente {
 	public static final String HOST = "localhost";
 	public static final int PORT = 27289;
 
-	public void tentandoConexao(){
-		
-		
-		
+	public void tentandoConexao() {
+		Thread tentandoConexao = new Thread();
+
+		tentandoConexao.start();
+
 	}
-	
-	public boolean testar(){
-	
-		
-		
+
+	public boolean testar() {
+		try {
+
+			Socket conexao = new Socket(HOST, 37389);
+
+		} catch (UnknownHostException e) {
+			System.out.println("Servidor Indisponivel");
+
+		} catch (IOException e) {
+			System.out.println("Erro de IO");
+			return false;
+
+		}
+
 		return true;
 	}
 
--- catraca_virtual/src/main/java/br/edu/unilab/catraca/verificador_de_conexao/TestClient.java
+++ catraca_virtual/src/main/java/br/edu/unilab/catraca/verificador_de_conexao/TestClient.java
@@ -4,6 +4,14 @@ public class TestClient {
 	
 	public static void main(String[] args) {
 		
+		Cliente c = new Cliente();
+		if(c.testar()){
+			
+			System.out.println("Valeu");
+		}
+		else{
+			System.out.println("Nao valeu");
+		}
 	}
 
 }
