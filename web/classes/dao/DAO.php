57a47a04238eecf539ef1bffb52b94d0a428191d
Jefferson Uchoa Ponte <jefponte@gmail.com>
2017-07-12 13:33:22 -0300

    Utilização de arquivo de configuração para melhor atender multiplas universidades.

--- web/classes/dao/DAO.php
+++ web/classes/dao/DAO.php
@@ -1,111 +1,84 @@
- <?php
- 
- /* Esse era producao
-  * Descomente esta e comente a de baixo para colocar em produção.
-  */ 
- 
- 
-// class DAO {
-// 	protected $conexao;
-// 	private $tipoDeConexao;
-	
-// 	public function DAO($conexao = null, $tipo = self::TIPO_DEFAULT) {
-// 		$this->tipoDeConexao = $tipo;
-// 		if ($conexao != null) {
-// 			$this->conexao = $conexao;
-// 		} else {
-			
-// 			$this->fazerConexao();
-// 		}
-// 	}
-// 	public function fazerConexao(){
-// 		switch ($this->tipoDeConexao) {
-// 			case self::TIPO_PG_DESENVOLVIMENTO:
-// 				$this->conexao = new PDO ( "pgsql:host=localhost dbname=treinamento user=catraca password=CaTraCa@unilab2015" );
-// 				break;
-// 			case self::TIPO_PG_PRODUCAO_LOCAL:
-// 					$this->conexao = new PDO ( "pgsql:host=localhost dbname=producao user=catraca password=CaTraCa@unilab2015" );
-// 					//$this->conexao = new PDO ( "pgsql:host=localhost dbname=treinamento user=catraca password=CaTraCa@unilab2015" );
+<?php
 
-// 					break;
-// 			case self::TIPO_PG_PRODUCAO_BAHIA:
-// 						$this->conexao = new PDO ( "pgsql:host=200.128.19.11 dbname=producao user=catraca password=CaTraCa@unilab2015" );
-// 						break;
-// 			case self::TIPO_PG_SIGAAA ://Produ��o do SIG. 
-// 				$this->conexao = new PDO ( "pgsql:host=200.129.19.80 dbname=sigaa user=catraca password=c4Tr@3a" );
-// 				break;
-// 			case self::TIPO_PG_SISTEMAS_COMUM ://Produ��o do SIG. 
-// 				$this->conexao = new PDO ( "pgsql:host=200.129.19.80 dbname=sistemas_comum user=catraca password=c4Tr@3a" );
-// 				break;
-// 			default :
-// 				$this->conexao = new PDO ( "pgsql:host=localhost dbname=desenvolvimento user=catraca password=CaTraCa@unilab2015" );
-// 				break;
-// 		}
-// 	}
-// 	public function setConexao($conexao) {
-// 		$this->conexao = $conexao;
-// 	}
-// 	public function getConexao() {
-// 		return $this->conexao;
-// 	}
-// 	public function fechaConexao() {
-// 		$this->conexao = null;
-// 	}
-// 	public function getTipoDeConexao(){
-// 		return $this->tipoDeConexao;
-// 	}
-// 	public function setTipoDeConexao($tipo){
-// 		$this->tipoDeConexao = $tipo;
-// 	}
-	
-
-// 	const TIPO_PG_SIGAAA = 4;
-// 	const TIPO_PG_SISTEMAS_COMUM = 6;
-// 	const TIPO_PG_PRODUCAO_LOCAL = 7;
-// 	const TIPO_PG_DESENVOLVIMENTO = 8;
-// 	const TIPO_PG_PRODUCAO_BAHIA = 9;
-// 	const TIPO_DEFAULT = self::TIPO_PG_PRODUCAO_LOCAL;
-	
-// }
-
-
-
-
-?>
-
- <?php
-
-
+/*********
+  * Copyright (c) 12/07/2017 {INITIAL COPYRIGHT UNILAB} {OTHER COPYRIGHT LABPATI/DISUP/DTI}.
+  * All rights reserved. This program and the accompanying materials
+  * are made available under the terms of the Eclipse Public License v1.0
+  * which accompanies this distribution, and is available at
+  * http://www.eclipse.org/legal/epl-v10.html
+  *
+  * Contributors:
+  *    Jefferson Uchôa Ponte - initial API and implementation and/or initial documentation
+  *********/
 class DAO {
+	const ARQUIVO_CONFIGURACAO = "/dados/sites/adm/catraca/config/config_bd.ini";
+	const TIPO_PRODUCAO = 0;
+	const TIPO_HOMOLOGACAO = 1;
+	const TIPO_AUTENTICACAO = 2;
+	const TIPO_USUARIOS = 3;
+	
+	// Altere essa constante para acessar base de produção
+	const TIPO_DEFAULT = self::TIPO_PRODUCAO;
 	protected $conexao;
 	private $tipoDeConexao;
-	
 	public function DAO(PDO $conexao = null, $tipo = self::TIPO_DEFAULT) {
 		$this->tipoDeConexao = $tipo;
 		if ($conexao != null) {
 			$this->conexao = $conexao;
 		} else {
 			
-			$this->fazerConexao();
+			$this->fazerConexao ();
 		}
 	}
-	public function fazerConexao(){
+	public function fazerConexao() {
+		$config = parse_ini_file ( self::ARQUIVO_CONFIGURACAO );
+		
 		switch ($this->tipoDeConexao) {
-			case self::TIPO_PG_DESENVOLVIMENTO:
-				$this->conexao = new PDO ( "pgsql:host=localhost dbname=homologacao user=catraca password=CaTraCa@unilab2015" );
+			case self::TIPO_PRODUCAO :
+				$bd ['sgdb'] = $config ['producao_sgbd'];
+				$bd ['nome'] = $config ['producao_bd_nome'];
+				$bd ['host'] = $config ['producao_host'];
+				$bd ['porta'] = $config ['producao_porta'];
+				$bd ['usuario'] = $config ['producao_usuario'];
+				$bd ['senha'] = $config ['producao_senha'];
 				break;
-			case self::TIPO_PG_PRODUCAO:
-					$this->conexao = new PDO ( "pgsql:host=localhost dbname=homologacao user=catraca password=CaTraCa@unilab2015" );
-					break;
-			case self::TIPO_PG_SIGAAA ://Produï¿½ï¿½o do SIG. 
-				$this->conexao = new PDO ( "pgsql:host=200.129.19.74 dbname=sigaa user=catraca password=c4Tr@3a" );
+			case self::TIPO_HOMOLOGACAO :
+				$bd ['sgdb'] = $config ['homologacao_sgdb'];
+				$bd ['nome'] = $config ['homologacao_bd_nome'];
+				$bd ['host'] = $config ['homologacao_host'];
+				$bd ['porta'] = $config ['homologacao_porta'];
+				$bd ['usuario'] = $config ['homologacao_usuario'];
+				$bd ['senha'] = $config ['homologacao_senha'];
 				break;
-			case self::TIPO_PG_SISTEMAS_COMUM ://Produï¿½ï¿½o do SIG. 
-				$this->conexao = new PDO ( "pgsql:host=200.129.19.74 dbname=sistemas_comum user=catraca password=c4Tr@3a" );
+			case self::TIPO_USUARIOS :
+				$bd ['sgdb'] = $config ['usuarios_sgdb'];
+				$bd ['nome'] = $config ['usuarios_bd_nome'];
+				$bd ['host'] = $config ['usuarios_host'];
+				$bd ['porta'] = $config ['usuarios_porta'];
+				$bd ['usuario'] = $config ['usuarios_usuario'];
+				$bd ['senha'] = $config ['usuarios_senha'];
+				break;
+			case self::TIPO_AUTENTICACAO :
+				$bd ['sgdb'] = $config ['autenticacao_sgdb'];
+				$bd ['nome'] = $config ['autenticacao_bd_nome'];
+				$bd ['host'] = $config ['autenticacao_host'];
+				$bd ['porta'] = $config ['autenticacao_porta'];
+				$bd ['usuario'] = $config ['autenticacao_usuario'];
+				$bd ['senha'] = $config ['autenticacao_senha'];
 				break;
 			default :
-				$this->conexao = new PDO ( "pgsql:host=localhost dbname=homologacao user=catraca password=CaTraCa@unilab2015" );
+				$bd ['sgdb'] = $config ['homologacao_sgdb'];
+				$bd ['nome'] = $config ['homologacao_bd_nome'];
+				$bd ['host'] = $config ['homologacao_host'];
+				$bd ['porta'] = $config ['homologacao_porta'];
+				$bd ['usuario'] = $config ['homologacao_usuario'];
+				$bd ['senha'] = $config ['homologacao_senha'];
 				break;
+		}
+		if ($bd ['sgdb'] == "postgres") {
+			$this->conexao = new PDO ( 'pgsql:host=' . $bd ['host'] . ' dbname=' . $bd ['nome'] . ' user=' . $bd ['usuario'] . ' password=' . $bd ['senha'] );
+		} else if ($bd ['sgdb'] == "mssql") {
+			$this->conexao = new PDO ( 'dblib:host=' . $bd ['host'] . ';dbname=' . $bd ['nome'], $bd ['usuario'], $bd ['senha'] );
 		}
 	}
 	public function setConexao($conexao) {
@@ -117,20 +90,12 @@ class DAO {
 	public function fechaConexao() {
 		$this->conexao = null;
 	}
-	public function getTipoDeConexao(){
+	public function getTipoDeConexao() {
 		return $this->tipoDeConexao;
 	}
-	public function setTipoDeConexao($tipo){
+	public function setTipoDeConexao($tipo) {
 		$this->tipoDeConexao = $tipo;
 	}
-	
-
-	const TIPO_PG_SIGAAA = 4;
-	const TIPO_PG_SISTEMAS_COMUM = 6;
-	const TIPO_PG_PRODUCAO = 7;
-	const TIPO_PG_DESENVOLVIMENTO = 8;
-	const TIPO_DEFAULT = self::TIPO_PG_DESENVOLVIMENTO;
-	
 }
 
 ?>
