e1485ebf344ff0302f6f5f02499d44d68b8497b8
Jefferson Uchoa Ponte <jefponte@gmail.com>
2017-09-18 13:41:27 -0300

    Master como está em produção.

--- web/index.php
+++ web/index.php
@@ -1,9 +1,8 @@
-
 <?php
 
-
-
-
+ini_set ( 'display_errors', 1 );
+ini_set ( 'display_startup_erros', 1 );
+error_reporting ( E_ALL );
 
 function __autoload($classe) {
 	if (file_exists ( 'classes/dao/' . $classe . '.php' ))
