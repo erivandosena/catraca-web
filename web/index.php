7ecd9d2aee150428835c97ea2145c4ff87e3f34d
Jefferson Uchoa Ponte <jefponte@gmail.com>
2017-09-19 09:05:14 -0300

    Partir da master.

--- web/index.php
+++ web/index.php
@@ -1,8 +1,9 @@
+
 <?php
 
-ini_set ( 'display_errors', 1 );
-ini_set ( 'display_startup_erros', 1 );
-error_reporting ( E_ALL );
+
+
+
 
 function __autoload($classe) {
 	if (file_exists ( 'classes/dao/' . $classe . '.php' ))
